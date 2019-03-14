0ea449d4b6386bc36711ff0bdea8d707592633fd
Artem Mironov <mironov.bam@gmail.com>
2019-03-14 21:15:13 +0400

    Added markup for dropdown component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from './filters/Filters';
 export * from './pagination/Pagination';
 export * from './playlist/Playlist';
 export * from './items-per-page/ItemsPerPage';
+export * from './dropdown/Dropdown';
