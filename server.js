74205dc6fa5e2999110f036bf64e44892a5d57bd
Artem Mironov <mironov.bam@gmail.com>
2019-03-16 10:28:24 +0400

    Added some libs. Removed unused.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-var fma = require('free-music-archive');
 
 app.use(cors());
 
